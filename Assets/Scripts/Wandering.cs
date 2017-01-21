51d2211fe8e4a8519c9cc8395fe76adff5cc9c8b
Valery Meleshkin <valery.meleshkin@gmail.com>
2017-01-21 21:07:06 +0100

    smarter wandering behaviour that is able to get out of narrow corridors

--- Assets/Scripts/Wandering.cs
+++ Assets/Scripts/Wandering.cs
@@ -7,12 +7,17 @@ public class Wandering : MonoBehaviour {
 	public Collider bounds;
 	public float wanderSpeed;
 	public float maxWanderDist;
+
 	private Vector3 _wanderTarget;
+	private Vector3 _prevPos;
 	private Rigidbody _body = null;
+	private float _approxSize = 1.0f;
 
 	void Start () {
 		_wanderTarget = this.transform.position;
+		_prevPos = _wanderTarget;
 		_body = GetComponent<Rigidbody>();
+		_approxSize = bounds.bounds.extents.magnitude;
 	}
 
 	void OnDisable() {
@@ -22,18 +27,35 @@ public class Wandering : MonoBehaviour {
 	void FixedUpdate () {
 		Vector3 selfPos = _body.transform.position;
 		_wanderTarget.y = selfPos.y;
-		if (Vector3.Distance(_wanderTarget, bounds.ClosestPointOnBounds(_wanderTarget)) <= 0.01f) {
-			float r1 = Random.Range (-maxWanderDist, maxWanderDist);
-			float r2 = Random.Range (-maxWanderDist, maxWanderDist);
-			Vector3 randomDir = new Vector3(r1, 0.0f, r2).normalized;
-			RaycastHit hit;
-			if (Physics.Raycast (selfPos, randomDir, out hit, maxWanderDist)) {
-				_wanderTarget = hit.point;
-			} else {
-				_wanderTarget = selfPos + maxWanderDist * randomDir;
-			}
+		if (Vector3.Distance(_wanderTarget, _prevPos) <= _approxSize) {
+			_wanderTarget = PickDirection ();
 		}				
 
+		_prevPos = selfPos;
 		_body.MovePosition (Vector3.MoveTowards (selfPos, _wanderTarget, wanderSpeed * Time.deltaTime));
+	}
+		
+
+	private Vector3 PickDirection() {
+		Vector3 selfPos = _body.transform.position;
+		float r1 = Random.Range (-maxWanderDist, maxWanderDist);
+		float r2 = Random.Range (-maxWanderDist, maxWanderDist);
+		Vector3 dir = new Vector3(r1, 0.0f, r2).normalized;
+		RaycastHit hit;
+
+		for (int i = 0; i < 1000; i++) {
+			if (Physics.Raycast (selfPos, dir, out hit, maxWanderDist)) {
+				if (Vector3.Distance (hit.point, selfPos) > _approxSize * 4.0f) {
+					return hit.point;
+				} else {
+					Vector3 temp = Vector3.Cross(hit.normal, dir);
+					dir = Vector3.Cross(temp, hit.normal);
+				}
+			} else {
+				return selfPos + maxWanderDist * dir;
+			}
+		}
+
+		return selfPos;
 	}
 }
