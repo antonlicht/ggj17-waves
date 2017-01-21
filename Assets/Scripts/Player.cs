0e56397725b7d560c0e244dd8a2080e2a2066bef
antonlicht <anton.licht@gmail.com>
2017-01-21 18:12:45 +0100

    Add collider to player and change time rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
+  public Rigidbody Rigidbody;
   public Animator Animator;
 
   public float Speed = 70f;
@@ -18,7 +19,7 @@ public class Player : MonoBehaviour
   {
     Vector2 velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * Speed;
 
-    transform.localPosition += (transform.forward * velocity.y + transform.right * velocity.x) * Time.deltaTime;
+    Rigidbody.MovePosition(transform.position + ((transform.forward * velocity.y + transform.right * velocity.x) * Time.deltaTime));
 
     Animator.SetFloat ("Horizontal", velocity.x);
     Animator.SetFloat ("Vertical", velocity.y);
@@ -28,7 +29,7 @@ public class Player : MonoBehaviour
   {
     float sinRot = Mathf.Sin (transform.position.z * 0.005f) * Mathf.Rad2Deg;
     float cosRot = Mathf.Cos (-transform.position.x * 0.005f) * Mathf.Rad2Deg;
-    float linRot = (transform.position.x + transform.position.z + Time.time * 100) * 0.05f;
+    float linRot = (transform.position.x + transform.position.z + Mathf.Sin (Time.time) * 50) * 0.05f;
     transform.eulerAngles = new Vector3 (0f, sinRot + cosRot + linRot, 0f);
   }
 }
