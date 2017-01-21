dcebaadbd9f020e30ce3962456c6a3b039a941f3
Valery Meleshkin <valery.meleshkin@gmail.com>
2017-01-21 15:23:23 +0100

    2d chase

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	public Collider2D areaOfEffect;
+	public Collider2D areaOfDamage;
+	public string targetTag;
+	public float speed;
+
+	private GameObject _chaseTarget = null;
+	private Rigidbody2D _body = null;
+
+	void Start () {
+		_chaseTarget = null;
+		_body = GetComponent<Rigidbody2D>();
+		areaOfEffect.isTrigger = true;
+		areaOfDamage.isTrigger = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (_chaseTarget)
+			return;
+		if (!other.CompareTag(targetTag))
+			return;
+
+		_chaseTarget = other.gameObject;
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		OnTriggerEnter2D (other);
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (_chaseTarget == other.gameObject) {
+			_chaseTarget = null;
+		}
+	}
+
+	void FixedUpdate () {
+		if (_chaseTarget) {			
+			Vector3 targetPos = _chaseTarget.transform.position;
+			Vector3 selfPos = this.transform.position;			
+			_body.MovePosition (Vector3.MoveTowards (selfPos, targetPos, speed * Time.deltaTime));
+		}
+	}
+}
---
+++ Assets/Scripts/kbmove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kbmove : MonoBehaviour {
+
+	public float speed = 0.4f;
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
+
+		this.transform.position += speed * Time.deltaTime * movement;
+	}
+}
