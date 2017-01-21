6260731ff4bb063f559cebf33e96f5ad47ce0cd4
antonlicht <anton.licht@gmail.com>
2017-01-21 12:06:13 +0100

    Implement simple touch inputs

---
+++ Assets/Player.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player : MonoBehaviour
+{
+  public PointerListener TouchInput;
+  public float MinSpeed = 10f;
+  public float SpeedThreshold = 2f;
+  public float MaxSpeed = 30f;
+
+  private Vector3 velocity;
+
+  void OnEnable ()
+  {
+    AddListeners ();
+  }
+
+  void OnDisable ()
+  {
+    TouchInput.RemoveAllListeners ();
+  }
+
+  void Update ()
+  {
+    transform.localPosition += velocity * Time.deltaTime;
+  }
+
+
+  private void AddListeners ()
+  {
+    TouchInput.onDrag.AddListener (HandleDrag);
+    TouchInput.onEndDrag.AddListener (HandleEndDrag);
+  }
+
+  public void HandleDrag (PointerEventData eventData)
+  {
+    Vector2 delta = eventData.position - eventData.pressPosition;
+    Vector3 newVelocity = (Mathf.Abs(delta.y) > Mathf.Abs(delta.x) ? transform.up * delta.y : transform.right * delta.x);
+
+    if ((newVelocity.magnitude < MinSpeed - SpeedThreshold && velocity.magnitude > 0) || (newVelocity.magnitude < MinSpeed + SpeedThreshold && velocity.magnitude == 0))
+    {
+      velocity = Vector3.zero;
+    }
+    else
+    {
+      if (newVelocity.magnitude > MaxSpeed)
+      {
+        newVelocity = newVelocity.normalized * MaxSpeed;
+      }
+
+      velocity = newVelocity;
+    }
+  }
+
+  private void HandleEndDrag (PointerEventData eventData)
+  {
+    velocity = Vector3.zero;
+  }
+
+}
---
+++ Assets/PointerListener.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PointerListener : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+  public PointerListenerGroup onClick = new PointerListenerGroup ();
+  public PointerListenerGroup onPointerDown = new PointerListenerGroup ();
+  public PointerListenerGroup onPointerUp = new PointerListenerGroup ();
+  public PointerListenerGroup onBeginDrag = new PointerListenerGroup ();
+  public PointerListenerGroup onEndDrag = new PointerListenerGroup ();
+  public PointerListenerGroup onDrag = new PointerListenerGroup ();
+  public void OnPointerDown (PointerEventData eventData)
+  {
+    onPointerDown.InvokeAll (eventData);
+  }
+
+  public void OnPointerUp (PointerEventData eventData)
+  {
+    onPointerUp.InvokeAll (eventData);
+  }
+
+  public void OnPointerClick (PointerEventData eventData)
+  {
+    onClick.InvokeAll (eventData);
+  }
+
+  public void OnBeginDrag (PointerEventData eventData)
+  {
+    onBeginDrag.InvokeAll (eventData);
+  }
+
+  public void OnEndDrag (PointerEventData eventData)
+  {
+    onEndDrag.InvokeAll (eventData);
+  }
+
+  public void OnDrag (PointerEventData eventData)
+  {
+    onDrag.InvokeAll (eventData);
+  }
+
+  public void RemoveAllListeners ()
+  {
+    onClick.RemoveAllListeners ();
+    onPointerDown.RemoveAllListeners ();
+    onPointerUp.RemoveAllListeners ();
+  }
+}
+
+public class PointerListenerGroup
+{
+  private List<Action<PointerEventData>> _listeners = new List<Action<PointerEventData>> ();
+  public void AddListener (Action<PointerEventData> listener)
+  {
+    _listeners.Add (listener);
+  }
+
+  public void RemoveListener (Action<PointerEventData> listener)
+  {
+    _listeners.Remove (listener);
+  }
+
+  public void RemoveAllListeners ()
+  {
+    _listeners.Clear ();
+  }
+
+  public void InvokeAll (PointerEventData e)
+  {
+    foreach (var listener in _listeners.ToList ())
+    {
+      if (listener != null)
+      {
+        listener (e);
+      }
+    }
+  }
+}
