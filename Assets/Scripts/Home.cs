a8d820a5d8615a5da9cfbffd35346516fed0344e
Dominik Hübner <dominik.huebner@gmail.com>
2017-01-21 18:32:43 +0100

    Home 2nd

--- Assets/Scripts/Home.cs
+++ Assets/Scripts/Home.cs
@@ -11,7 +11,6 @@ public class Home : MonoBehaviour
 
   private void OnTriggerEnter(Collider other)
   {
-    Debug.Log(other.gameObject);
     if (other.gameObject.GetComponentInParent<Player>() != null)
     {
       Debug.Log("you won!");
