7043e456649667dc74e41ad0c0c1952d79721d57
Dominik Hübner <dominik.huebner@gmail.com>
2017-01-22 01:11:25 +0100

    Home is where the golden cat is

--- Assets/Scripts/Home.cs
+++ Assets/Scripts/Home.cs
@@ -3,16 +3,44 @@ using UnityEngine.SceneManagement;
 
 public class Home : MonoBehaviour
 {
+  public Wandering wandering;
+  public Animator animator;
+
+  private float pauseTime;
+  
   private void Start()
   {
     var circlePos = Random.insideUnitSphere * 10000;
     circlePos.y = 0;
     transform.position = circlePos;
+
+    pauseTime = Random.Range(2, 10);
+  }
+
+  private void Update()
+  {
+    pauseTime -= Time.deltaTime;
+    if (pauseTime < 0)
+    {
+      if (wandering.enabled)
+      {
+        wandering.enabled = false;
+        animator.SetBool("idle", true);        
+      }
+      else if (pauseTime < -1)
+      {
+        wandering.enabled = true;
+        animator.SetBool("idle", false);
+        pauseTime = Random.Range(2, 10);
+      }
+    }
+    
+    
   }
 
   private void OnTriggerEnter(Collider other)
-  {
-    if (other.gameObject.GetComponentInParent<Player>() != null)
+  {    
+    if (other.gameObject.GetComponentInParent<Player>() != null && other.name != "CatSpawnRadius")
     {
       Debug.Log("you won!");
 //      SceneManager.LoadScene(SceneManager.GetActiveScene().name);
