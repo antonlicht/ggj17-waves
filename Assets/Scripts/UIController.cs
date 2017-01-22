80da166406366b38a5938853146d565844f5f9e6
antonlicht <anton.licht@gmail.com>
2017-01-22 10:55:38 +0100

    Show won screen

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+  public Player Player;
+  public Slider Slider;
+  public GameObject WonScreen;
+  public GameObject LostScreen;
+
+	void Update ()
+  {
+		Slider.value = Player.health;
+	}
+
+  public void ShowWonScreen()
+  {
+    WonScreen.SetActive(true);
+  }
+}
